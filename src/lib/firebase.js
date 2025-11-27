23f6faf3d7f8534efd21d5691f4362083d3f11b3
Maricel Csc <dairacruz59@gmail.com>
2025-11-26 21:16:39 -0500

    Agregar

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -8,13 +8,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBsnw53RMdq7aXz_XBIZOXHp7QcMENrk5Q",
-  authDomain: "let-s-cook-22f58.firebaseapp.com",
-  projectId: "let-s-cook-22f58",
-  storageBucket: "let-s-cook-22f58.firebasestorage.app",
-  messagingSenderId: "690337970384",
-  appId: "1:690337970384:web:b25270e767280032a2c6d5",
-  measurementId: "G-5D8DB8SST5",
+  apiKey: "AIzaSyBED7Sz9I9k-C0JOQGBtqzL-YhumftRDW4",
+  authDomain: "cocina-3cfa6.firebaseapp.com",
+  projectId: "cocina-3cfa6",
+  storageBucket: "cocina-3cfa6.firebasestorage.app",
+  messagingSenderId: "210528107330",
+  appId: "1:210528107330:web:94be14af8c15777a7ee6db",
 };
 
 const app = initializeApp(firebaseConfig);
